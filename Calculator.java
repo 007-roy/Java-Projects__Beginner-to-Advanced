75e157a40bf7217533f9a73dcc6b61a0674a0304
Satyen <satyenroy0071@gmail.com>
2024-11-25 18:40:21 +0600

    Second Project of Java Beginner Projects

---
+++ Calculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Calculator {
+    
+    public static void add(int x,int y){
+        System.out.println(x + y);
+    }
+
+    public static void multiply(int x,int y){
+        System.out.println(x * y);
+    }
+
+    public static void subtract(int x,int y){
+        System.out.println(x - y);
+    }
+
+    public static void divide(int x,int y){
+        System.out.println(x / y);
+    }
+
+
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print(" What mathematical operation do you want to do ?");
+        System.out.println("Press 1 to Add, 2 to Subtract, 3 to Multiply, 4 to Divide.");
+
+        int o = scn.nextInt();
+
+        System.out.println("Please enter your values x and y.");
+
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+
+        if ( o == 1) {
+            Calculator.add(x, y);
+        }else if (o == 2) {
+            Calculator.subtract(x, y);
+        }else if (o == 3) {
+            Calculator.multiply(x, y);
+        }else if (o == 4) {
+            Calculator.divide(x, y);
+        }
+    
+
+
+    }
+
+
+
+}
